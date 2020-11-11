c4f4add96fb49d389ac8c0f7706807633d77e693
zhanghui <2050470637@qq.com>
2020-11-12 00:50:39 +0800

    初始导入

--- top_user_parent/top_user_server_01/src/main/java/cn/top/UserProvider01.java
+++ top_user_parent/top_user_server_01/src/main/java/cn/top/UserProvider01.java
@@ -1,11 +1,15 @@
 package cn.top;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages="cn.top.mapper")
+@EnableFeignClients(basePackages="cn.top.client")
 public class UserProvider01 {
     public static void main(String[] args) {
         SpringApplication.run(UserProvider01.class, args);
